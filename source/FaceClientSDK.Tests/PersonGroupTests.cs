c1863c430fd32a17f5000ab03c1238858f4395b4
Roberto Cervantes <rcervantes@outlook.com>
2018-10-08 11:48:07 -0500

    Adding PersonGroup and PersonGroupPerson logic to API reference and unit tests

---
+++ source/FaceClientSDK.Tests/PersonGroupTests.cs
@@ -0,0 +1,225 @@
+using FaceClientSDK.Domain.PersonGroup;
+using FaceClientSDK.Tests.Fixtures;
+using System.Collections.Generic;
+using Xunit;
+
+namespace FaceClientSDK.Tests
+{
+    public class PersonGroupTests : IClassFixture<FaceAPISettingsFixture>
+    {
+        private FaceAPISettingsFixture faceAPISettingsFixture = null;
+
+        public PersonGroupTests(FaceAPISettingsFixture fixture)
+        {
+            faceAPISettingsFixture = fixture;
+
+            APIReference.FaceAPIKey = faceAPISettingsFixture.FaceAPIKey;
+            APIReference.FaceAPIZone = faceAPISettingsFixture.FaceAPIZone;
+        }
+
+        [Fact]
+        public async void CreateAsyncTest()
+        {
+            bool result = false;
+            var identifier = System.Guid.NewGuid().ToString();
+
+            try
+            {
+                result = await APIReference.Instance.PersonGroup.CreateAsync(identifier, identifier, identifier);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                var deletion_result = await APIReference.Instance.PersonGroup.DeleteAsync(identifier);
+            }
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async void DeleteAsyncTest()
+        {
+            bool result = false;
+            var identifier = System.Guid.NewGuid().ToString();
+
+            try
+            {
+                var creation_result = await APIReference.Instance.PersonGroup.CreateAsync(identifier, identifier, identifier);
+                result = await APIReference.Instance.PersonGroup.DeleteAsync(identifier);
+            }
+            catch
+            {
+                throw;
+            }
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async void GetAsyncTest()
+        {
+            GetResult result = null;
+            var identifier = System.Guid.NewGuid().ToString();
+
+            try
+            {
+                var creation_result = await APIReference.Instance.PersonGroup.CreateAsync(identifier, identifier, identifier);
+
+                if (creation_result)
+                    result = await APIReference.Instance.PersonGroup.GetAsync(identifier);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                var deletion_result = await APIReference.Instance.PersonGroup.DeleteAsync(identifier);
+            }
+
+            Assert.True(result != null);
+        }
+
+        [Fact]
+        public async void GetTrainingStatusAsyncTest()
+        {
+            GetTrainingStatusResult result = null;
+            var identifier = System.Guid.NewGuid().ToString();
+
+            try
+            {
+                var creation_result = await APIReference.Instance.PersonGroup.CreateAsync(identifier, identifier, identifier);
+
+                bool training_result = false;
+                training_result = await APIReference.Instance.PersonGroup.TrainAsync(identifier);
+
+                if (training_result)
+                {
+                    while (true)
+                    {
+                        System.Threading.Tasks.Task.Delay(1000).Wait();
+                        result = await APIReference.Instance.PersonGroup.GetTrainingStatusAsync(identifier);
+
+                        if (result.status == "running")
+                        {
+                            continue;
+                        }
+                        else if (result.status == "succeeded")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                var deletion_result = await APIReference.Instance.PersonGroup.DeleteAsync(identifier);
+            }
+
+            Assert.True(result != null);
+        }
+
+        [Fact]
+        public async void ListAsyncTest()
+        {
+            List<ListResult> result = null;
+            var identifier = System.Guid.NewGuid().ToString();
+
+            try
+            {
+                var creation_result = await APIReference.Instance.PersonGroup.CreateAsync(identifier, identifier, identifier);
+
+                if (creation_result)
+                    result = await APIReference.Instance.PersonGroup.ListAsync(string.Empty, 1000);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                var deletion_result = await APIReference.Instance.PersonGroup.DeleteAsync(identifier);
+            }
+
+            Assert.True(result != null);
+        }
+
+        [Fact]
+        public async void TrainAsyncTest()
+        {
+            bool result = false;
+            var identifier = System.Guid.NewGuid().ToString();
+
+            try
+            {
+                var creation_result = await APIReference.Instance.PersonGroup.CreateAsync(identifier, identifier, identifier);
+                result = await APIReference.Instance.PersonGroup.TrainAsync(identifier);
+
+                while (true)
+                {
+                    System.Threading.Tasks.Task.Delay(1000).Wait();
+                    var status = await APIReference.Instance.PersonGroup.GetTrainingStatusAsync(identifier);
+
+                    if (status.status == "running")
+                    {
+                        continue;
+                    }
+                    else if (status.status == "succeeded")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                var deletion_result = await APIReference.Instance.PersonGroup.DeleteAsync(identifier);
+            }
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async void UpdateAsyncTest()
+        {
+            bool result = false;
+            var identifier = System.Guid.NewGuid().ToString();
+
+            try
+            {
+                var creation_result = await APIReference.Instance.PersonGroup.CreateAsync(identifier, identifier, identifier);
+
+                if (creation_result)
+                    result = await APIReference.Instance.PersonGroup.UpdateAsync(identifier, "Name", "User Data Sample");
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                var deletion_result = await APIReference.Instance.PersonGroup.DeleteAsync(identifier);
+            }
+
+            Assert.True(result);
+        }
+    }
+}
---
+++ source/FaceClientSDK/Interfaces/IPersonGroupPerson.cs
@@ -0,0 +1,27 @@
+using FaceClientSDK.Domain.PersonGroupPerson;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FaceClientSDK.Interfaces
+{
+    public interface IPersonGroupPerson
+    {
+        Task<AddFaceResult> AddFaceAsync(string largePersonGroupId, string personId, string url, string userData, string targetFace);
+
+        Task<CreateResult> CreateAsync(string largePersonGroupId, string name, string userData);
+
+        Task<bool> DeleteAsync(string largePersonGroupId, string personId);
+
+        Task<bool> DeleteFaceAsync(string largePersonGroupId, string personId, string persistedFaceId);
+
+        Task<GetResult> GetAsync(string largePersonGroupId, string personId);
+
+        Task<GetFaceResult> GetFaceAsync(string largePersonGroupId, string personId, string persistedFaceId);
+
+        Task<List<ListResult>> ListAsync(string largePersonGroupId, string start, int top);
+
+        Task<bool> UpdateAsync(string largePersonGroupId, string personId, string name, string userData);
+
+        Task<bool> UpdateFaceAsync(string largePersonGroupId, string personId, string persistedFaceId, string userData);
+    }
+}
