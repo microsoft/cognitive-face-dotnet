a9cdc209a67f10130686d9d65b9ca35a7d297833
Roberto Cervantes <rcervantes@outlook.com>
2018-10-08 11:48:07 -0500

    Adding PersonGroup and PersonGroupPerson logic to API reference and unit tests

---
+++ source/FaceClientSDK/Domain/PersonGroupPerson/GetFaceResult.cs
@@ -0,0 +1,8 @@
+namespace FaceClientSDK.Domain.PersonGroupPerson
+{
+    public class GetFaceResult
+    {
+        public string persistedFaceId { get; set; }
+        public string userData { get; set; }
+    }
+}
